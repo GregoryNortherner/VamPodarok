a44a13c404ea464e6be0bfd0f2c0ee6d9e5d99f5
GregoryNortherner <Slash.Ninaro@yandex.ru>
2017-12-05 14:37:13 +0400

    VamPodarok v3.1.0

---
+++ js/function.js
@@ -0,0 +1,55 @@
+// функция гамбургер меню
+
+// @var
+var hamburgerClass = 'hamburger';
+var hamburgerClassActive = 'is-active';
+var menu = '.slider-main-text-bottom ul';
+var toggleMenu = 'left';
+
+var hamburgerObj = jQuery( '.' + hamburgerClass );
+var menuObj = jQuery( menu );
+
+// активация гамбургера
+function activeHamburger() {
+    hamburgerObj.addClass( hamburgerClassActive );
+    hamburgerObj.css( 'zIndex', '999999').css('position', 'fixed');
+    hoverDocument();
+}
+
+// деактивация гамбургера
+function notActiveHamburger() {
+    hamburgerObj.removeClass( hamburgerClassActive );
+    hoverDocument('drop');
+}
+
+function getToggleMenu() {
+    if ( typeof toggleMenu !== "undefined" ) {
+        if ( toggleMenu === 'left' ) {
+            menuObj.toggle( 'slide' );
+            menuObj.css( 'zIndex', '999999').css('position', 'fixed');
+        }
+    } else {
+        menuObj.slideToggle();
+    }
+}
+
+function hoverDocument( type ) {
+    var hover = '<div class="hover-document-menu" style="width: 100%; height: 100vh; background-color: #000000; opacity: 0.7;position: fixed; top: 0; left: 0; z-index: 999997"></div>';
+    if ( typeof type !== "undefined" ) {
+        if ( type === 'create' ) {
+            jQuery( 'body' ).prepend( hover );
+        }
+        if ( type === 'drop' ) {
+            jQuery( ".hover-document-menu" ).remove();
+        }
+    } else {
+        jQuery('body').prepend( hover );
+    }
+}
+
+// клик по гамбургеру
+jQuery( document ).on( 'click', '.' + hamburgerClass, function () {
+    hamburgerObj.hasClass( hamburgerClassActive ) ? notActiveHamburger() : activeHamburger();
+    getToggleMenu();
+
+});
